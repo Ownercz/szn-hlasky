68ebcd0a33c4f728301c63b02f3bd05ebe951211
Petr Krch <petr.krch@firma.seznam.cz>
2017-11-16 12:37:28 +0100

    del sleep

--- play.sh
+++ play.sh
@@ -3,7 +3,6 @@
 cd /home/krch/szn-hlasky/
 ls $(ls *.mp3 | cut -f1 -d _ | uniq | sed -n "$(echo $(( $RANDOM % $(ls *.mp3 | cut -f1 -d _ | uniq | wc -l) + 1 )))p")_* |sort -R |tail -$N |while read file; do
   echo $file
-  sleep 1
   cvlc -q --play-and-exit other/gongsluz.wav $file
   break
 done
