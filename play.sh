dbadde034b4ef06900c8c5f3d9821041209b3894
Petr Krch <petr.krch@firma.seznam.cz>
2017-11-16 12:35:47 +0100

    mp3gain

--- play.sh
+++ play.sh
@@ -2,9 +2,8 @@
 
 cd /home/krch/szn-hlasky/
 ls $(ls *.mp3 | cut -f1 -d _ | uniq | sed -n "$(echo $(( $RANDOM % $(ls *.mp3 | cut -f1 -d _ | uniq | wc -l) + 1 )))p")_* |sort -R |tail -$N |while read file; do
-  # Something involving $file, or you can leave
-  # off the while to just get the filenames
   echo $file
+  sleep 1
   cvlc -q --play-and-exit other/gongsluz.wav $file
   break
 done
