9a020f1d2235cd751c6a1c7c1fc9100c1a54ec71
Petr Krch <petr.krch@firma.seznam.cz>
2017-11-16 12:30:06 +0100

    fix proportion

--- play.sh
+++ play.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /home/krch/szn-hlasky/
-ls *.mp3 |sort -R |tail -$N |while read file; do
+ls $(ls *.mp3 | cut -f1 -d _ | uniq | sed -n "$(echo $(( $RANDOM % $(ls *.mp3 | cut -f1 -d _ | uniq | wc -l) + 1 )))p")_* |sort -R |tail -$N |while read file; do
   # Something involving $file, or you can leave
   # off the while to just get the filenames
   echo $file
-  cvlc -q --play-and-exit other/gongsluz.wav $file
+#  cvlc -q --play-and-exit other/gongsluz.wav $file
   break
 done
