59390b29386697786209adb45953b6f3b43e7d77
Petr Krch <petr.krch@firma.seznam.cz>
2017-12-13 09:12:11 +0100

    move mp3 and clear

--- play.sh
+++ play.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd /home/krch/szn-hlasky/
-ls $(ls *.mp3 | cut -f1 -d _ | uniq | sed -n "$(echo $(( $RANDOM % $(ls *.mp3 | cut -f1 -d _ | uniq | wc -l) + 1 )))p")_* |sort -R |tail -$N |while read file; do
+num_types=$(ls ./mp3/ | cut -f1 -d _ | uniq | wc -l)
+random_type_num=$(echo $(( $RANDOM % $num_types + 1 )))
+random_type_name=$(ls ./mp3/ | cut -f1 -d _ | uniq | sed -n "${random_type_num}p")
+ls ./mp3/${random_type_name}_* |sort -R |tail -$N |while read file; do
   echo $file
-  cvlc -q --play-and-exit other/gongsluz.wav $file
+  cvlc -q --play-and-exit ./other/gongsluz.wav $file
   break
 done
