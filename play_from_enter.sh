68b7f6e1f03876a72399f1fcebaee8b289dfaac2
Petr Krch <petr.krch@firma.seznam.cz>
2018-01-24 14:02:39 +0100

    guys

--- play_from_enter.sh
+++ play_from_enter.sh
@@ -3,6 +3,15 @@
 num_types=$(ls ./mp3_for_enter/ | cut -f1 -d _ | uniq | wc -l)
 random_type_num=$(echo $(( $RANDOM % $num_types + 1 )))
 random_type_name=$(ls ./mp3_for_enter/ | cut -f1 -d _ | uniq | sed -n "${random_type_num}p")
+declare -A guys=(
+["10.0.132.26"]="gaba"
+["10.0.132.28"]="lada"
+["172.24.1.107"]="vidlak"
+)
+if [[ ${guys[$SOCAT_PEERADDR]} ]]; then
+  cvlc -q --play-and-exit ./other/${guys[$SOCAT_PEERADDR]}.mp3
+fi
+echo "no - $SOCAT_PEERADDR" >> /tmp/odkoho
 ls ./mp3_for_enter/${random_type_name}_* |sort -R |tail -$N |while read file; do
   echo $file
   cvlc -q --play-and-exit $file
